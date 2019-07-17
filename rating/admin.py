25260cdb0259df50481519342947697c3165feca
Barnabas <bh@pulilab.com>
2019-07-17 13:39:44 +0200

    remove ObjectRatingAdmin

--- rating/admin.py
+++ rating/admin.py
@@ -1,9 +1,1 @@
-from django.contrib import admin
-from .models import ObjectRating
-
-
-class ObjectRatingAdmin(admin.ModelAdmin):
-    list_display = ['user', 'user_type']
-
-
-admin.site.register(ObjectRating, ObjectRatingAdmin)
+# from django.contrib import admin
